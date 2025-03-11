53bb053074c817c91ce16b179f418d44bf5b74fd
Sho Kaneko <kanekoshoyu@gmail.com>
2025-03-11 15:03:54 +0800

    data source trait

--- src/adapter/mod.rs
+++ src/adapter/mod.rs
@@ -13,4 +13,25 @@ pub mod error;
 // PolygonIO
 pub mod polygonio;
 
-pub use error::AdapterError;
+pub use error::{AdapterError, AdapterResult};
+
+#[derive(Clone, Debug)]
+pub enum DataSourceType {
+    Historic,
+    Stream,
+}
+
+#[async_trait::async_trait]
+pub trait DataSource {
+    type DataType;
+    /// arbitrary id that identified data source
+    /// it can be generative but has to be replicable
+    fn id() -> String;
+    /// source type
+    fn data_source_type() -> DataSourceType;
+}
+
+#[async_trait::async_trait]
+pub trait HistoricDataSource: DataSource {
+    async fn fetch() -> AdapterResult<Self::DataType>;
+}
